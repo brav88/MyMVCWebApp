58ec8e6248fbaec54364342d8c09bf4b38ec5186
brav88 <brav88@hotmail.com>
2025-01-30 20:58:48 -0600

    Agregar archivos de proyecto.

---
+++ MyWebApp/Controllers/LoginController.cs
@@ -0,0 +1,99 @@
+﻿using Firebase.Auth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApp.Firebase;
+
+namespace MyWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: LoginController
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            try
+            {
+                UserCredential userCredential = await FirebaseAuthHelper.setFirebaseAuthClient().SignInWithEmailAndPasswordAsync(email, password);
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return RedirectToAction("Error", "Home");
+            }
+        }
+
+        // GET: LoginController/Details/5
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: LoginController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: LoginController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LoginController/Edit/5
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: LoginController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: LoginController/Delete/5
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: LoginController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MyWebApp/Firebase/FirebaseAuthHelper.cs
@@ -0,0 +1,26 @@
+﻿using Firebase.Auth.Providers;
+using Firebase.Auth;
+
+namespace MyWebApp.Firebase
+{
+    public static class FirebaseAuthHelper
+    {
+        public const string firebaseAppId = "";
+        public const string firebaseApiKey = "";
+
+        public static FirebaseAuthClient setFirebaseAuthClient()
+        {
+            var auth = new FirebaseAuthClient(new FirebaseAuthConfig()
+            {
+                ApiKey = firebaseApiKey,
+                AuthDomain = firebaseAppId,
+                Providers = new FirebaseAuthProvider[]
+                {
+                    new EmailProvider()
+                }
+            });
+
+            return auth;
+        }
+    }
+}
